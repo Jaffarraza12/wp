15bc57f1e290c39f52c715ab3e030d8fb522ef27
Jaffar Raza <jaffaraza@gmail.com>
2019-10-15 17:58:31 +0500

    Module Update.

--- wp-content/themes/davinci2/header.php
+++ wp-content/themes/davinci2/header.php
@@ -37,6 +37,7 @@
     $info    = $ADSTM[ 'info' ];
     print_r($product );
 
+    }
 
     ?>
        <!--<meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
@@ -44,9 +45,6 @@
         <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
         <meta property="og:description"        content="How much does culture influence creative thinking?" />
         <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />-->
-    }
-
-    ?>
 
 
 
